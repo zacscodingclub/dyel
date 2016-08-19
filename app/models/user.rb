f0717632d1266277390392b3f30ddf04e0198b05
zacscodingclub <zbaston@gmail.com>
2016-08-19 08:35:13 -0500

    add user details to show page, wire up User and Workout associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_many :workouts
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
